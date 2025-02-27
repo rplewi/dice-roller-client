5c756c12e4d31f44e36b0faf8076ff06daeb8050
rplewi <rlewi0506@gmail.com>
2025-02-26 21:46:54 -0600

    TEST

--- index.js
+++ index.js
@@ -1,29 +1,10 @@
-
 async function rollDice(){
-    await fetch("http://localhost:3000/roll")
-    .then((response) => {
-        return response.json()
-    })
-    .then((data) => {
-        document.getElementById("dice1").value = data.dice1
-        document.getElementById("dice2").value = data.dice2
-        document.getElementById("dice3").value = data.dice3
-        document.getElementById("dice4").value = data.dice4
-        document.getElementById("dice5").value = data.dice5
-        document.getElementById("dice6").value = data.dice6
-    })
-    
+    const response = await fetch("http://localhost:3000/roll")
+    const data = await response.json()
+    document.getElementById("dice1").value = data.die1
+    document.getElementById("dice2").value = data.die2
+    document.getElementById("dice3").value = data.die3
+    document.getElementById("dice4").value = data.die4
+    document.getElementById("dice5").value = data.die5
+    document.getElementById("dice6").value = data.die6
 }
-
-
-
-async function setNumbers(data){
-    document.getElementById("dice1").value = data.dice1
-    document.getElementById("dice2").value = data.dice2
-    document.getElementById("dice3").value = data.dice3
-    document.getElementById("dice4").value = data.dice4
-    document.getElementById("dice5").value = data.dice5
-    document.getElementById("dice6").value = data.dice6
-}
-
-
